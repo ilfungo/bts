1b27115f11d7887f4c5e7eeec559248de38b5f57
simoneLazzaro <simone.lazzaroict@gmail.com>
2015-02-18 16:58:47 +0100

    override elenco prodotti per eliminare il prezzo (loop pagina)

---
+++ wp-content/themes/wootique-child/woocommerce/loop/price.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Loop Price  (prezzo elenco prodotti)
+ *
+ * @author 		simone
+ * @package 	WooCommerce/Templates
+ * @version     1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+global $product;
